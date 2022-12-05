54ea23e857d00ca2951b929c3ac761fa384a4123
Henrique Fantini <henrique.fantini@hotmail.com>
2022-12-05 08:54:54 -0300

    Starting a window through GLFW lib

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,33 @@
 #include<iostream>
+#include<Windows.h>
+#include<glfw3.h>
+
+const int WINDOW_WIDTH = 800;
+const int WINDOW_HEIGHT = 600;
 
 int main()
 {
-	std::cout << "Hello World! :)" << std::endl;
+	if (glfwInit() == GLFW_FALSE)
+	{
+		MessageBoxW(NULL, L"Failed at init GLFW environment...", L"Ops!", MB_OK | MB_ICONERROR);
+		glfwTerminate();
+		return 1;
+	}
+
+	GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "BLUE MARBLE - HF", nullptr, nullptr);
+	if (window == nullptr)
+	{
+		MessageBoxW(NULL, L"Failed at create GLFW Window...", L"Ops!", MB_OK | MB_ICONERROR);
+		glfwTerminate();
+		return 1;
+	}
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+		glfwSwapBuffers(window);
+	}
+
+	glfwTerminate();
 	return 0;
 }
