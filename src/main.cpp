0b2c72205624942b5f9226281d743903b9c547e9
Henrique Fantini <henrique.fantini@hotmail.com>
2022-12-09 12:27:33 -0300

    Working with transformation matrices

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
-#include<iostream>
-#include<Windows.h>
-#include<glew.h>
-#include<glfw3.h>
-#include<string>
+#include <iostream>
+#include <Windows.h>
+#include <glew.h>
+#include <glfw3.h>
+#include <string>
 #include <sstream>
+#include <array>
+#include <glm.hpp>
+#include <ext.hpp>
 
 const int WINDOW_WIDTH = 800;
 const int WINDOW_HEIGHT = 600;
@@ -47,15 +50,74 @@ int main()
 
 	glfwSetWindowTitle(window, title.c_str());
 
-	glClearColor(0.0, 0.0, 1.0, 0.0);
+	// TRIANGLE VERTEX
+
+	std::array<glm::vec3, 3> triangle =
+	{
+		glm::vec3 {-1.0f, -1.0f, 0.0f},
+		glm::vec3 {1.0f, -1.0f, 0.0f},
+		glm::vec3 {0.0f, 1.0f, 0.0f}
+	};
+
+	// MODEL MATRIX
+
+	glm::mat4 modelMatrix = glm::identity<glm::mat4>();
+
+	// VIEW MATRIX
+
+	glm::vec3 eye{ 0.0f, 0.0f, -10.0f };
+	glm::vec3 center{ 0.0f, 0.0f, 00.0f };
+	glm::vec3 up{ 0.0f, 1.0f, 0.0f };
+	glm::mat4 viewMatrix = glm::lookAt(eye, center, up);
+
+	// PROJECTION MATRIX
+
+	constexpr float fov = glm::radians(45.0f);
+	const float aspectRatio = WINDOW_WIDTH / WINDOW_HEIGHT;
+	const float fNear = 0.001f;
+	const float fFar = 1000.0f;
+
+	glm::mat4 projectionMatrix = glm::perspective(fov, aspectRatio, fNear, fFar);
+
+	// MODEL VIEW PROJECTION (MADE BY THE CPU FOR TESTING PURPOSES)
+
+	glm::mat4 modelViewProjectionMatrix = projectionMatrix * viewMatrix * modelMatrix;
+
+	// APPLYING THE modelViewProjectionMatrix IN EACH TRIANGLE VERTEX
+
+	for (glm::vec3& vertex : triangle)
+	{
+		glm::vec4 projectedVertex = modelViewProjectionMatrix * glm::vec4{ vertex, 1.0f };
+		projectedVertex = projectedVertex / projectedVertex.w;
+		vertex = projectedVertex;
+	}
+
+
+	GLuint vertexBuffer;
+	glGenBuffers(1, &vertexBuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(triangle), triangle.data(), GL_STATIC_DRAW);
+
+	glClearColor(0.0f, 0.0f, 1.0f, 0.0f);
 
 	while (!glfwWindowShouldClose(window))
 	{
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		glEnableVertexAttribArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+		
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		glDisableVertexAttribArray(0);
+
 		glfwPollEvents();
 		glfwSwapBuffers(window);
 	}
+
+	glDeleteBuffers(1, &vertexBuffer);
 
 	glfwTerminate();
 	return 0;
